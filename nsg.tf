2e8cd450c474c7a094746d78ace7fa3e38c9371a
konstantinou77 <kostiakryvulia@gmail.com>
2025-05-17 17:16:33 +0300

    fix_nsg_format

--- nsg.tf
+++ nsg.tf
@@ -24,14 +24,14 @@ resource "azurerm_network_security_group" "example" {
 
     for_each = local.security_rules
     content {
-      name                   = security_rule.value.name
-      priority               = security_rule.value.priority
-      direction              = security_rule.value.direction
-      access                 = security_rule.value.access
-      protocol               = security_rule.value.protocol
-      source_port_range      = security_rule.value.source_port_range
-      destination_port_range = security_rule.value.destination_port_range
-      source_address_prefix  = security_rule.value.source_address_prefix
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
       destination_address_prefix = security_rule.value.destination_address_prefix
     }
   }
